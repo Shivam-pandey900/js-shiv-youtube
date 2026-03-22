02d448e6a56c3a092c9f121720f4d40867ebef5c
Shivam-pandey900 <113529564+Shivam-pandey900@users.noreply.github.com>
2026-03-22 16:56:57 +0000

    added datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const userId=100
+var userName = "Shivam"
+let userCity = "Giridih"
+userState = "Jharkhand"
+
+// userId = 200
+let city;
+
+console.log(userId);
+
+/*
+Prefer not use var
+because of issue in functional scope and block scope
+*/
+
+userName = "SitaRam"
+userCity = "Jamua"
+userState = "up"
+
+console.table([userId, userName, userCity, userState, city])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all js code as newer version
+
+// alert(3 + 3)  // we are using nodejs not browser so error will come.
+
+console.log(3
+    + 3
+) // code readability should be high.
+
+// datatypes:
+
+let name = "shivam"
+let no = 100
+let isLoggedIn = false
+let state;
+
+// different datatypes: 
+
+// number 2 to power 53
+// for larger number we use - bigint
+// string= ""
+// boolean => true/false
+// null - standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null); // object
