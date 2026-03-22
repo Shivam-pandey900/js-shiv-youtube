c30e53654959165e57b2fd52fb16970c69aae21a
Shivam-pandey900 <113529564+Shivam-pandey900@users.noreply.github.com>
2026-03-22 16:22:44 +0000

    understood variables concept

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const userId=100
+var userName = "Shivam"
+let userCity = "Giridih"
+userState = "Jharkhand"
+
+// userId = 200
+
+console.log(userId);
+
+userName = "SitaRam"
+userCity = "Jamua"
+userState = "up"
+
+console.table([userId, userName, userCity, userState])
+
