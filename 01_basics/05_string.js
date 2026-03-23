13579916677d19f741b112a44da29a6c8e0ba119
Shivam-pandey900 <113529564+Shivam-pandey900@users.noreply.github.com>
2026-03-23 17:26:42 +0000

    string done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,35 @@
+const name = "shivam"
+const repocount = 2
+
+// console.log(name + repocount + " value")   // this is older one , in production we donot use it
+// back tick is used in production system with ${} operator to define variables.
+
+// console.log(`My name is ${name} , and my total repocount is ${repocount}`)
+
+const str = new String("shivam-pandey-kumar-radheshyam")
+
+// console.log(str);
+// console.log(str.anchor());
+// console.log(str.at(1));
+// console.log(str.big());
+// console.log(str.blink());
+// console.log(str.bold());
+
+// console.log(str.charAt(4));
+// console.log(str.indexOf('a'));
+
+// console.log(str.endsWith('a'));
+// console.log(str.toUpperCase());
+// console.log(str.replace('pandey','kumar'));
+// console.log(str.split('-'));
+// console.log(str);
+console.log(str.trim());
+
+console.log(str.substring(0,12));
+
+console.log(str.slice(-10,4));
+
+console.log(str.includes('kumar'));
+console.log(str.concat("shivam"));
+
+console.log(str.valueOf());
