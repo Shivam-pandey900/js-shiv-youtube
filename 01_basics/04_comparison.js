4abdbb6cfc7844774434ced28a6255668b55fbb2
Shivam-pandey900 <113529564+Shivam-pandey900@users.noreply.github.com>
2026-03-23 14:27:25 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+console.log(null == 0);
+console.log(null < 0);
+console.log(null >= 0);
+
+console.log("2" == 2); // internally "2" converted to number
+console.log("02" == 2);
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+// ===  this operator not only checks value but first it checks datatypes of the two operands
+
+console.log("2" === 2);
+console.log(2 === 2);
+
