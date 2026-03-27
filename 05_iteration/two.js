997cf0a713e007c141dda939d06fd34017c01974
Shivam-pandey900 <113529564+Shivam-pandey900@users.noreply.github.com>
2026-03-27 11:37:44 +0000

    while and do while done

---
+++ 05_iteration/two.js
@@ -0,0 +1,19 @@
+let ind = 1
+
+// while (ind < 10) {
+//     console.log(`value is ${ind}`);
+//     ind += 2
+// }
+
+let myarr = ['hi', 'hello', 'world']
+
+let i = 0
+// while (i < myarr.length) {
+//     console.log(`value is ${myarr[i]}`)
+//     i++;
+// }
+
+do{
+    console.log(`value is ${ind}`);
+    ind = ind + 2
+} while(ind < 10)
